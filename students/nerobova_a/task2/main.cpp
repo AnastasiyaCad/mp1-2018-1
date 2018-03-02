1552906a6d6f750f789d60aa726dd5511060061b
AnastasiyaCad <nastwink@yandex.ru>
2018-03-02 16:11:54 +0300

    Закончен класс

--- students/nerobova_a/task2/main.cpp
+++ students/nerobova_a/task2/main.cpp
@@ -3,33 +3,43 @@
 #include <stdio.h>
 using namespace std;
 
+
 class  Polinom
 {
 	int n;
 	double *koef;
 public:
-	int Input_n();
 	int Output_n(int n);
-	int Output_koef(int i); 
-	void Input_koef();
-	void Calculate();
+	double Output_koef(int i); 
+	double Calculate(double x);
+	void Output();
+	void Proiz();
 
 	Polinom() 
 	{
 		n = 0;
 		koef = new double[n + 1];
 		koef[0] = 0;
-	};
-	Polinom(int _n, double *_koef)
-	{
-		n = _n;
-		koef = new double[_n + 1];
-		for (int i = 0; i <= _n; i++)
-			koef[i] = _koef[i];
-	};
+	}
+
+	Polinom(int _n, double _koef[]) :n(_n), koef(_koef) {}
 
 	Polinom(const Polinom &p);
-	Polinom::~Polinom();
+	~Polinom();
+
+	Polinom& operator=(const Polinom& obj)
+	{
+		if (this == &obj)
+			return *this;
+		if (koef != obj.koef) {
+			delete[] koef;
+			koef = new double[n + 1];
+		}
+		for (int i = 0; i <= n; i++)
+			koef[i] = obj.koef[i];
+		koef = obj.koef;
+		return *this;
+	}
 };
 
 Polinom::Polinom(const Polinom &p)
@@ -45,60 +55,84 @@ Polinom::~Polinom()
 	delete[] koef;
 }
 
-int Polinom::Input_n()
-{
-	cout << "n = ";
-	cin >> n;
-	return n;
-}
 
-void Polinom::Input_koef()
-{
-	koef = new double[n + 1]; 
-	for (int i = 0; i <= n; i++)
-	{
-		cout << "k" << i << " = ";
-		cin >> koef[i];
-	}
-}
-
-int Polinom::Output_koef(int i)
+double Polinom::Output_koef(int i)
 {
 	return koef[i];
 };
 
-void Polinom::Calculate()
+double Polinom::Calculate(double x)
 {
-	int x;
-	cout << "x = ";
-	cin >> x;
-	int s = 0;
+	
+	double s = 0;
 	int tmp = n;
 	double p;
 
 	for (int i = 0; i <= n; i++)
 	{
-		p = (x^n) *  koef[i];
+		p = pow(x,n-i) *  koef[i];
 		s = s + p;
-		tmp--;
 	}
-
+	return s;
 };
+
 
 int Polinom::Output_n(int n)
 {
 	return n;
 }
 
+void Polinom::Output()
+{
+	cout << koef[0] << "(x^" << n << ")";
+	for (int i = 1; i < n; i++)
+	{
+		cout << " + " << koef[i] << "(x^" << n - i << ")";
+	}
+	cout << " + " << koef[n]<<"\n";
+}
 
+void Polinom::Proiz()
+{
+	double p;
+	double s;
+	int tmp;
+	tmp = n;
+	p = n * koef[0];
+	n--;
+	cout << p << "x^(" << n << ")";
+	for (int i = 1; i <= n; i++)
+	{
+		p = n * koef[i];
+		n--;
+		//s = s + p;
+		cout << " + " << p << "x^" << n << ")";
+	}
+	cout << " + " << koef[n-1];
+		
+}
 
 int main()
 {
 	int n;
-	
-	Polinom();
-	
-	
-	
+	double x;
+	cout << "n = ";
+	cin >> n;
+	double koef[13];
+
+	for (int i = 0; i <= n; i++)
+		{
+			cout << "k" << i << " = ";
+			cin >> koef[i];
+		}
+
+	Polinom p(n, koef);
+	p.Output();
+	cout << "x = ";
+	cin >> x;
+	cout << "\n";
+	cout << p.Calculate(x)<<"\n";
+	p.Proiz();
+	system("pause");
 
 }
