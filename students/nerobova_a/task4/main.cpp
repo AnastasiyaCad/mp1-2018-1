2dd1bb4b01adca5479a65bd14307d0ad8f7fcca2
AnastasiyaCad <nastwink@yandex.ru>
2018-03-31 15:41:41 +0300

    Дополнения

--- students/nerobova_a/task4/main.cpp
+++ students/nerobova_a/task4/main.cpp
@@ -10,10 +10,10 @@ using namespace std;
 class Thermometer;
 class Date
 {
+public:
 	int num;
 	int month;
 	int year;
-public:
 	Date() : year(0), month(0), num(0) {}
 	Date(int _year, int _month, int _num): year(_year), month(_month), num(_num){} 
 	friend Thermometer;
@@ -73,8 +73,8 @@ Date::~Date()
 
 class Time
 {
-	int hour;
 public:
+	int hour;
 	Time(): hour(0){}
 	Time(int _hour): hour(_hour){}
 	friend Thermometer;
@@ -133,7 +133,8 @@ public:
 		t = th.t;
 		degree = th.degree;
 		return *this;
-	}
+	}	
+	friend ostream &operator<<(ostream& os, const Thermometer &th);
 	int GetDateDegree();
 	void SetDateDegree(int _degree);
 	int GetMonth(Date _d);
@@ -144,12 +145,23 @@ public:
 	int GetHour(Time _t);
 	friend Date;
 	friend Time;
-	void SetDegree(Date _d, Time _t, int _degree);
+
+	void SetDegree(Date _d, Time _t, int _degree); //наюл.дение
 	void SetSeveralDegree(Date _d, int _degree);
-	int GetDegree(Date _d, Time _t);
-	double GetAverage(Date _d);
-	int GetCase(int _month);
+	int GetDegree(Date _d, Time _t); // вернуть тепературу
+	double GetAverage(); //средн€€ вообще
+	int GetCase(int _month); //числа дл€ подсчета дней за мес€ц
+	double GetAverageMonth(int month); //средн€€ за мес€ц
+	double GetAverageDate(Date _d); //средн€€ за день
+	double GetAverageNight(int month); // средн€€ ночна€
+	double GetAverageDay(int month); // средн€€ дневна€
+	void SaveToFile(int k);
+	int Test();
+	Date GetD();
+	Time GetT();
 };
+
+
 
 void Thermometer::SetDateDegree(int _degree)
 {
@@ -166,9 +178,10 @@ Thermometer::Thermometer(const Thermometer &th)
 	t = th.t;
 	degree = th.degree;
 }
+
 Thermometer::~Thermometer()
 {
-
+	
 }
 
 int Thermometer::GetMonth(Date _d)
@@ -183,8 +196,24 @@ int Thermometer::GetYear(Date _d)
 {
 	return _d.GetDateYear();
 }
+
+Date Thermometer::GetD()
+{
+	d.num;
+	d.month;
+	d.year;
+	return d;
+}
+
+Time Thermometer::GetT()
+{
+	t.hour;
+	return t;
+}
+
 int Thermometer::GetHour(Time _t)
 {
+	_t.hour; 
 	return _t.GetDateHour();
 }
 int Thermometer::GetNum1(Date _d)
@@ -234,6 +263,15 @@ int Thermometer::GetN(Date _d)
 	return Num;
 } 
 
+int Thermometer::Test()
+{
+	int k = 0;
+	for (int i = 0; i < values.size(); i++)
+		for (int j = 0; j < 24; j++)
+					k++;
+	return k;
+}
+
 void Thermometer::SetDegree(Date _d, Time _t, int _degree) // 3
 {
 	values[GetN(_d)][_t.GetDateHour()] = _degree;
@@ -252,27 +290,75 @@ void Thermometer::SetSeveralDegree(Date _d, int _degree) // 5
 	}
 }
 
-double Thermometer::GetAverage(Date _d)
+double Thermometer::GetAverageDate(Date _d)
 {
-	//1
-	int k = 0;
+	double k = 0;
 	for (int i = 0; i < 24; i++)
 		k = k + values[GetN(_d)][i];
 	k = k / 24;
 	return k;
-	//2
-	for (int i = GetCase(GetMonth(_d)); i < GetCase(GetMonth(_d)+1); i++)
-		for(int j = 0; j < 24; j++)
-		k = k + values[i][j];
-	k = k / (GetCase(GetMonth(_d) + 1) - GetCase(GetMonth(_d)));
+}
+
+double Thermometer::GetAverageMonth(int month)
+{
+	double k = 0;
+	for (int i = GetCase(month); i < GetCase(month + 1); i++)
+		for (int j = 0; j < 24; j++)
+			k = k + values[i][j];
+	k = k / (GetCase(month + 1) - GetCase(month));
 	return k;
-	//3
+}
+double Thermometer::GetAverage()
+{
+	double k = 0;
 	for (int i = 0; i < values.size(); i++)
 		for (int j = 0; j < 24; j++)
 		{
 			k = k + values[i][j];
 		}
-	k = k / 365;
+	k = k / Test();
+	return k;
+}
+
+double Thermometer::GetAverageNight(int month)
+{
+	double k = 0;
+	for (int i = GetCase(month); i < GetCase(month + 1); i++)
+		for (int j = 0; j < 3; j++)
+			k = k + values[i][j];
+	for (int i = GetCase(month); i < GetCase(month + 1); i++)
+		for (int j = 16; j < 24; j++)
+			k = k + values[i][j];
+	k = k / (GetCase(month + 1) - GetCase(month));
+	return k;
+}
+
+double Thermometer::GetAverageDay(int month) 
+{
+	double k = 0;
+	for (int i = GetCase(month); i < GetCase(month + 1); i++)
+		for (int j = 4; j < 16; j++)
+			k = k + values[i][j];
+	k = k / (GetCase(month + 1) - GetCase(month));
+	return k;
+}
+
+ofstream Thermometer_in;
+ifstream Thermometer_out;
+ostream &operator<<(ostream& os, const Thermometer &th)
+{
+	os << th.d.num  << '.' << th.d.month << '.' << th.d.year << '.' << th.t.hour << '.' << th.degree;
+	return os;
+}
+
+void Thermometer::SaveToFile(int k)
+{
+	Thermometer_in << "Degree Counts: " << k << endl;
+	for (int i = 0; i < k; i++)
+		for (int j = 0; j < 24; j++)
+		{
+			Thermometer_in <<  << endl << endl << values[i][j] << endl << endl;
+		}
 }
 
 void main()
@@ -345,9 +431,12 @@ void main()
 			cin >> degree;
 			tr.SetSeveralDegree(d, degree);
 		}
-		
-		
 		cout << "\n";
+		int k;
+		k = tr.Test();
+		Thermometer_in.open("C:\Games\Temp.txt");
+		tr.SaveToFile(k);
+		Thermometer_in.close();
 	}
 	}
 	system("pause");
